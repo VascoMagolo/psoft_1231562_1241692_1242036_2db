f6180702e97bad9755dabbddbf2dcab91b3e3525
Vasco Magolo <vascomagolo@gmail.com>
2026-04-28 16:20:25 +0100

    Started pumls, refractored some things for DDD

--- src/main/java/aisafe/aircrafts/infrastructure/AircraftModelController.java
+++ src/main/java/aisafe/aircrafts/infrastructure/AircraftModelController.java
@@ -1,7 +1,8 @@
 package aisafe.aircrafts.infrastructure;
 
+import aisafe.aircrafts.application.ListAircraftModelsUseCase;
+import aisafe.aircrafts.application.RegisterAircraftModelUseCase;
 import aisafe.aircrafts.domain.AircraftModel;
-import aisafe.services.AircraftModelService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,22 +12,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/aircraftModels")
 public class AircraftModelController {
-    private final AircraftModelService service;
-
-    public AircraftModelController(AircraftModelService service) {
-        this.service = service;
+    private final RegisterAircraftModelUseCase RegisterAircraftModel;
+    private final ListAircraftModelsUseCase ListAircraftModels;
+    public AircraftModelController(RegisterAircraftModelUseCase registerAircraftModel, ListAircraftModelsUseCase listAircraftModels) {
+        this.RegisterAircraftModel = registerAircraftModel;
+        this.ListAircraftModels = listAircraftModels;
     }
 
     @PostMapping
     public ResponseEntity<AircraftModel> createModel(@RequestBody AircraftModel model) {
-        AircraftModel savedModel = service.registerAircraftModel(model);
-
+        AircraftModel savedModel = RegisterAircraftModel.execute(model);
         return new ResponseEntity<>(savedModel, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<AircraftModel>> getAllModels() {
-        List<AircraftModel> models = service.findAll();
+        List<AircraftModel> models = ListAircraftModels.execute();
         return ResponseEntity.ok(models);
     }
 }
